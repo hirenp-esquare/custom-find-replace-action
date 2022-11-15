3faa8e502276c0f582dd25a70d00852a9905d491
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2022-11-15 11:35:57 +0530

    added

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ try {
     if (err) {
       console.log('Error', err);
     } else {
-      console.log(res);
+      console.log(`File list: ${res}`);
+
       for (let index = 0; index < res.length; index++) {
         const path = res[index]
         findAndReplace(path)
