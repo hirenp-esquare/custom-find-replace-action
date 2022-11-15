dfb0f91b5f3cb3c3397038b6e572ae8520727c81
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2022-11-15 11:38:14 +0530

    changed glob

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 
 var getDirectories = function (src, callback) {
-  glob(src + '/**/*.md', callback);
+  glob(src, callback);
 };
 
 function findAndReplace(path) {
@@ -24,7 +24,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  getDirectories('.', function (err, res) {
+  getDirectories('**/*.md', function (err, res) {
     if (err) {
       console.log('Error', err);
     } else {
